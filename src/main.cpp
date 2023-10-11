b71de427e60b337636fcc28b81cf246de8847d44
3k2ng <tung09102002@gmail.com>
2023-10-11 06:13:00 -0600

    core game finished

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ const Group PLAYER_GROUP = Group(
 		0x9f, 0x9f, 0x0f, 0x9f, 0x9f,
 		0x00, 0x9f, 0x9f, 0x9f, 0x00
 	}
-);
+	);
 
 const Group BOSS_WING_BASE = Group(
 	16, 0, 1,
@@ -65,7 +65,7 @@ const Group BOSS_WING_BASE = Group(
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x17, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x9c, 0x9c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 	}
-);
+	);
 
 const Group BOSS_WING_COVER = Group(
 	16, 0, 6,
@@ -83,7 +83,7 @@ const Group BOSS_WING_COVER = Group(
 
 		0x00, 0x00, 0x9e, 0x9e, 0x00, 0x00, 0x00, 0x9e, 0x9e, 0x00, 0x00, 0x00, 0x9e, 0x9e, 0x00, 0x00,
 	}
-);
+	);
 
 const Group BOSS_BODY_BASE = Group(
 	14, 0, 0,
@@ -133,7 +133,7 @@ const Group BOSS_BODY_COVER = Group(
 		0x9e, 0x9e, 0x9e, 0x9e, 0x9e, 0x9e, 0x9e, 0x9e,
 		0x9e, 0x9e, 0x9e, 0x9e, 0x9e, 0x9e, 0x9e, 0x9e,
 	}
-);
+	);
 
 struct Screen {
 	Texture2D cp437_8x8;
@@ -159,9 +159,9 @@ struct Screen {
 		}
 	}
 
-	void DrawGroup(int x, int y, const Group& group) {
+	void DrawGroup(int x, int y, const Group& group, bool override_color = false, const unsigned char &color_to_override = 0xbf) {
 		for (int i = 0; i < group.codepoints.size(); ++i) {
-			DrawTile(x + group.offset_x + i % group.width, y + group.offset_y + i / group.width, group.codepoints[i], group.colors[i]);
+			DrawTile(x + group.offset_x + i % group.width, y + group.offset_y + i / group.width, group.codepoints[i], override_color ? color_to_override : group.colors[i]);
 		}
 	}
 
@@ -220,49 +220,231 @@ struct Screen {
 };
 
 struct Bullet {
-	int pos_x, pos_y, dir_x, dir_y, speed, accel, maxspeed;
+	float pos_x, pos_y, angle, speed, accel, maxspeed;
 	bool capped;
-	int cd;
-	Bullet(int pos_x, int pos_y, int dir_x, int dir_y, int speed, int accel = 0, int maxspeed = 0) :
+	Bullet(float pos_x, float pos_y, float angle, float speed, float accel = 0, float maxspeed = 0) :
 		pos_x(pos_x),
 		pos_y(pos_y),
-		dir_x(dir_x),
-		dir_y(dir_y),
+		angle(angle),
 		speed(speed),
 		accel(accel),
 		maxspeed(maxspeed),
 		capped(accel >= 0 ? (speed >= maxspeed) : (speed <= maxspeed))
-	{
-		cd = FRAME_PER_SECOND;
-	}
+	{}
 
 	void Update() {
 		if (!capped) {
 			speed += accel;
 			capped = accel >= 0 ? (speed >= maxspeed) : (speed <= maxspeed);
 		}
-		if (cd <= 0) {
-			if (speed >= 0) {
-				pos_x += dir_x;
-				pos_y += dir_y;
-			}
-			else {
-				pos_x -= dir_x;
-				pos_y -= dir_y;
-			}
-			cd = FRAME_PER_SECOND;
+
+		pos_x += cosf(angle) * speed;
+		pos_y -= sinf(angle) * speed;
+	}
+
+	int GetX() const { return static_cast<int>(pos_x + 0.5f); }
+	int GetY() const { return static_cast<int>(pos_y + 0.5f); }
+};
+
+struct Boss {
+	static constexpr int TOTAL_HEALTH = 1200;
+	static constexpr int BODY_COVER_HEALTH = 200;
+	static constexpr int WING_HEALTH = 400;
+	static constexpr int WING_COVER_HEALTH = 200;
+
+
+	float pos_x, pos_y, vel_x, vel_y, acc_x, acc_y;
+
+	int total_health = TOTAL_HEALTH, body_cover_health = BODY_COVER_HEALTH, left_wing_health = WING_HEALTH, right_wing_health = WING_HEALTH;
+
+	enum class State
+	{
+		ENTERING,
+		LEFT, RIGHT,
+		FINAL_INTO_POSITION,
+		FINAL_SHOOTING
+	};
+	State state = State::ENTERING;
+
+	int state_cd = FRAME_PER_SECOND;
+	int wing_shot_cd = FRAME_PER_SECOND + 30;
+	int body_cover_shot_cd = FRAME_PER_SECOND;
+	int final_state_shot_cd = 0;
+
+	void Update() {
+		vel_x += acc_x;
+		vel_y += acc_y;
+
+		pos_x += vel_x;
+		pos_y += vel_y;
+
+		if (left_wing_health <= 0 && right_wing_health <= 0 && body_cover_health <= 0 && state != State::FINAL_INTO_POSITION && state != State::FINAL_SHOOTING) {
+			state = State::FINAL_INTO_POSITION;
+			state_cd = static_cast<float>(FRAME_PER_SECOND);
+			vel_x = 2 * (static_cast<float>(WIDTH) / 2 - 23 - pos_x) / state_cd;
+			vel_y = 2 * (static_cast<float>(HEIGHT) / 2 - 4 - pos_y) / state_cd;
+			acc_x = -vel_x / state_cd;
+			acc_y = -vel_y / state_cd;
 		}
-		cd -= abs(speed);
+
+		if (state_cd <= 0) {
+			if (state == State::ENTERING || state == State::RIGHT) {
+				state = State::LEFT;
+				vel_x = -0.20;
+				state_cd = 200;
+				acc_x = -2 * vel_x / state_cd;
+				vel_y = 0;
+				acc_y = 0;
+			}
+			else if (state == State::LEFT) {
+				state = State::RIGHT;
+				vel_x = 0.20;
+				state_cd = 200;
+				acc_x = -2 * vel_x / state_cd;
+				vel_y = 0;
+				acc_y = 0;
+			}
+			else if (state == State::FINAL_INTO_POSITION) {
+				state = State::FINAL_SHOOTING;
+				vel_x = 0;
+				vel_y = 0;
+				acc_x = 0;
+				acc_y = 0;
+			}
+		}
+
+		if (wing_shot_cd <= 0) {
+			wing_shot_cd = 30;
+		}
+
+		if (body_cover_shot_cd <= 0) {
+			body_cover_shot_cd = 80;
+		}
+
+		if (final_state_shot_cd <= 0) {
+			final_state_shot_cd = 10;
+		}
+
+		--state_cd;
+		--wing_shot_cd;
+		--body_cover_shot_cd;
+		--final_state_shot_cd;
+	}
+
+	std::vector<Bullet> Shoot(int player_x, int player_y) const {
+		std::vector<Bullet> to_shoot;
+		if (wing_shot_cd <= 0) {
+			if (left_wing_health > WING_HEALTH - WING_COVER_HEALTH) {
+				for (float i = 0; i < 3; ++i) {
+					to_shoot.emplace_back(pos_x + 2.f, pos_y + 9, -PI / 2.f - PI / 6.f, 0.4f + i * 0.05f);
+					to_shoot.emplace_back(pos_x + 7.f, pos_y + 9, -PI / 2.f, 0.4f + i * 0.05f);
+					to_shoot.emplace_back(pos_x + 12.f, pos_y + 9, -PI / 2.f + PI / 6.f, 0.4f + i * 0.05f);
+				}
+			}
+			else if (left_wing_health > 0) {
+				for (float i = 1; i < 7; i += 2) {
+					to_shoot.emplace_back(pos_x + 7.f, pos_y + 9, -i * PI / 7, 0.3f);
+					to_shoot.emplace_back(pos_x + 7.f, pos_y + 9, -i * PI / 7 - PI / 21, 0.3f);
+					to_shoot.emplace_back(pos_x + 7.f, pos_y + 9, -i * PI / 7 - 2 * PI / 21, 0.3f);
+					to_shoot.emplace_back(pos_x + 7.f, pos_y + 9, -i * PI / 7 - PI / 7, 0.3f);
+				}
+			}
+			if (right_wing_health > WING_HEALTH - WING_COVER_HEALTH) {
+				for (float i = 0; i < 3; ++i) {
+					to_shoot.emplace_back(pos_x + 32.f, pos_y + 9, -PI / 2.f - PI / 6.f, 0.4f + i * 0.05f);
+					to_shoot.emplace_back(pos_x + 37.f, pos_y + 9, -PI / 2.f, 0.4f + i * 0.05f);
+					to_shoot.emplace_back(pos_x + 42.f, pos_y + 9, -PI / 2.f + PI / 6.f, 0.4f + i * 0.05f);
+				}
+			}
+			else if (right_wing_health > 0) {
+				for (float i = 1; i < 7; i += 2) {
+					to_shoot.emplace_back(pos_x + 37.f, pos_y + 9, -i * PI / 7, 0.3f);
+					to_shoot.emplace_back(pos_x + 37.f, pos_y + 9, -i * PI / 7 - PI / 21, 0.3f);
+					to_shoot.emplace_back(pos_x + 37.f, pos_y + 9, -i * PI / 7 - 2 * PI / 21, 0.3f);
+					to_shoot.emplace_back(pos_x + 37.f, pos_y + 9, -i * PI / 7 - PI / 7, 0.3f);
+				}
+			}
+		}
+		if (body_cover_health > 0 && body_cover_shot_cd <= 0) {
+			to_shoot.emplace_back(pos_x + 23.f, pos_y + 16.f + 1, atan2f(pos_y + 16.f - static_cast<float>(player_y), static_cast<float>(player_x) - (pos_x + 23.f)), 0.4f);
+			to_shoot.emplace_back(pos_x + 23.f, pos_y + 16.f - 1, atan2f(pos_y + 16.f - static_cast<float>(player_y), static_cast<float>(player_x) - (pos_x + 23.f)), 0.4f);
+			to_shoot.emplace_back(pos_x + 23.f + 1, pos_y + 16.f, atan2f(pos_y + 16.f - static_cast<float>(player_y), static_cast<float>(player_x) - (pos_x + 23.f)), 0.4f);
+			to_shoot.emplace_back(pos_x + 23.f - 1, pos_y + 16.f, atan2f(pos_y + 16.f - static_cast<float>(player_y), static_cast<float>(player_x) - (pos_x + 23.f)), 0.4f);
+		}
+		if (state == State::FINAL_SHOOTING && final_state_shot_cd <= 0) {
+			constexpr float STAR_SHOTS = 16;
+			constexpr float ANGLE_PIECE = 2 * PI / STAR_SHOTS;
+			for (float i = 0; i < STAR_SHOTS; ++i) {
+				to_shoot.emplace_back(pos_x + 23.f, pos_y + 4.f, atan2f(pos_y + 4.f - static_cast<float>(player_y), static_cast<float>(player_x) - (pos_x + 23.f)) + i * ANGLE_PIECE, 0.5f);
+			}
+		}
+		return to_shoot;
+	}
+
+	bool CheckCollision(int x, int y) {
+		if (state == State::ENTERING) return false;
+
+		int rel_x = x - static_cast<int>(pos_x), rel_y = y - static_cast<int>(pos_y);
+
+		if (left_wing_health > 0 && rel_x >= 0 && rel_x < 16 && rel_y >= 1 && rel_y < 7) {
+			--left_wing_health;
+			if (left_wing_health <= 0) {
+				total_health -= WING_HEALTH;
+			}
+			return true;
+		}
+
+		if (right_wing_health > 0 && rel_x >= 30 && rel_x < 46 && rel_y >= 1 && rel_y < 7) {
+			--right_wing_health;
+			if (right_wing_health <= 0) {
+				total_health -= WING_HEALTH;
+			}
+			return true;
+		}
+
+		if (body_cover_health > 0 && rel_x >= 19 && rel_x < 27 && rel_y >= 8 && rel_y < 14) {
+			--body_cover_health;
+			if (body_cover_health <= 0) {
+				total_health -= BODY_COVER_HEALTH;
+			}
+			return true;
+		}
+		if (total_health > 0 && rel_x >= 19 && rel_x < 27 && rel_y >= 0 && rel_y < 8) {
+			--total_health;
+			return true;
+		}
+		return false;
+	}
+
+	void Draw(Screen& sc) {
+		sc.DrawGroup(pos_x + 16, pos_y, BOSS_BODY_BASE);
+		if (left_wing_health > 0) {
+			sc.DrawGroup(static_cast<int>(pos_x), static_cast<int>(pos_y), BOSS_WING_BASE);
+		}
+		if (left_wing_health > WING_HEALTH - WING_COVER_HEALTH) {
+			sc.DrawGroup(static_cast<int>(pos_x), static_cast<int>(pos_y), BOSS_WING_COVER);
+		}
+		if (right_wing_health > 0) {
+			sc.DrawGroup(static_cast<int>(pos_x) + 30, static_cast<int>(pos_y), BOSS_WING_BASE);
+		}
+		if (right_wing_health > WING_HEALTH - WING_COVER_HEALTH) {
+			sc.DrawGroup(static_cast<int>(pos_x) + 30, static_cast<int>(pos_y), BOSS_WING_COVER);
+		}
+		if (body_cover_health > 0) {
+			sc.DrawGroup(static_cast<int>(pos_x) + 16, static_cast<int>(pos_y), BOSS_BODY_COVER);
+		}
 	}
 };
 
 struct GameManager {
-	int player_x, player_y, player_lives;
+	int player_x = WIDTH / 2, player_y = HEIGHT - 10, player_lives = 3;
 
-	std::list<Bullet> bullets;
+	Boss boss{ WIDTH / 2 - 23, 0, 0, 0.30, 0, -0.005 };
+
+	std::list<Bullet> player_bullets;
+	std::list<Bullet> boss_bullets;
 	int shot_cd = 0;
-
-	GameManager(int player_x, int player_y, int player_lives) : player_x(player_x), player_y(player_y), player_lives(player_lives) {};
+	int iframe_cd = 0;
 
 	void Update() {
 		int dir_x = 0, dir_y = 0;
@@ -272,27 +454,76 @@ struct GameManager {
 		if (IsKeyDown(KEY_RIGHT)) ++dir_x;
 
 		if (IsKeyDown(KEY_C) and shot_cd <= 0) {
-			bullets.emplace_back(player_x - 1, player_y - 1, 0, -1, 60);
-			bullets.emplace_back(player_x, player_y - 2, 0, -1, 60);
-			bullets.emplace_back(player_x + 1, player_y - 1, 0, -1, 60);
+			player_bullets.emplace_back(player_x - 1, player_y - 1, PI / 2, 1);
+			player_bullets.emplace_back(player_x, player_y - 2, PI / 2, 1);
+			player_bullets.emplace_back(player_x + 1, player_y - 1, PI / 2, 1);
 			shot_cd = 5;
+		}
+
+		if (boss.total_health > 0) {
+			std::vector<Bullet> boss_bullets_to_spawn = boss.Shoot(player_x, player_y);
+			boss_bullets.insert(boss_bullets.end(), boss_bullets_to_spawn.begin(), boss_bullets_to_spawn.end());
 		}
 
 		player_x = std::min(std::max(player_x + dir_x, 1), static_cast<int>(WIDTH - 2));
 		player_y = std::min(std::max(player_y + dir_y, 1), static_cast<int>(HEIGHT - 2));
-		for (auto it = bullets.begin(); it != bullets.end(); ++it) {
-			it->Update();
-			//if (it->pos_x < 1 || it->pos_x > WIDTH - 2 || it->pos_y < 1 || it->pos_y > HEIGHT - 2) {
-			//	bullets.erase(it);
-			//}
+
+		if (boss.CheckCollision(player_x, player_y) && iframe_cd <= 0) {
+			--player_lives;
+			player_x = WIDTH / 2;
+			player_y = HEIGHT - 10;
+			iframe_cd = 2 * FRAME_PER_SECOND;
 		}
+
+		std::vector<std::list<Bullet>::iterator> player_bullets_to_remove;
+		for (auto it = player_bullets.begin(); it != player_bullets.end(); ++it) {
+			it->Update();
+			if (it->pos_x < 1 || it->pos_x > WIDTH - 2 || it->pos_y < 1 || it->pos_y > HEIGHT - 2) {
+				player_bullets_to_remove.push_back(it);
+			}
+			else if (boss.CheckCollision(it->pos_x, it->pos_y)) {
+				player_bullets_to_remove.push_back(it);
+			}
+		}
+		for (const std::list<Bullet>::iterator& it : player_bullets_to_remove) {
+			player_bullets.erase(it);
+		}
+
+		std::vector<std::list<Bullet>::iterator> boss_bullets_to_remove;
+		for (auto it = boss_bullets.begin(); it != boss_bullets.end(); ++it) {
+			it->Update();
+			if (it->pos_x < 1 || it->pos_x > WIDTH - 2 || it->pos_y < 1 || it->pos_y > HEIGHT - 2) {
+				boss_bullets_to_remove.push_back(it);
+			}
+			else if (it->GetX() == player_x && it->GetY() == player_y) {
+				boss_bullets_to_remove.push_back(it);
+				if (iframe_cd <= 0) {
+					--player_lives;
+					player_x = WIDTH / 2;
+					player_y = HEIGHT - 10;
+					iframe_cd = 2 * FRAME_PER_SECOND;
+				}
+			}
+		}
+		for (const std::list<Bullet>::iterator& it : boss_bullets_to_remove) {
+			boss_bullets.erase(it);
+		}
+
+		boss.Update();
+
 		--shot_cd;
+		--iframe_cd;
 	}
 
 	void Draw(Screen& sc) {
-		sc.DrawGroup(player_x, player_y, PLAYER_GROUP);
-		for (auto it = bullets.cbegin(); it != bullets.cend(); ++it) {
-			sc.DrawTile(it->pos_x, it->pos_y, 0x13, 0x37);
+		sc.DrawGroup(player_x, player_y, PLAYER_GROUP, (iframe_cd / 8) % 2 == 1);
+		if (boss.total_health > 0)
+			boss.Draw(sc);
+		for (const Bullet& player_bullet : player_bullets) {
+			sc.DrawTile(player_bullet.GetX(), player_bullet.GetY(), 0x13, 0x37);
+		}
+		for (const Bullet& boss_bullet : boss_bullets) {
+			sc.DrawTile(boss_bullet.GetX(), boss_bullet.GetY(), 0x04, 0x4f);
 		}
 		sc.DrawBorder(0xc9, 0xbb, 0xc8, 0xbc, 0xcd, 0xba, 0x9f);
 		sc.DrawText(" LIVES:  ", 3, HEIGHT - 1, 0xbf);
@@ -303,20 +534,15 @@ struct GameManager {
 int main()
 {
 	Screen sc("u tell me a Tung text-based this game jam");
-	GameManager g(WIDTH / 2, HEIGHT - 10, 3);
+	GameManager g;
 	SetTargetFPS(FRAME_PER_SECOND);
 	while (!WindowShouldClose())
 	{
-		g.Update();
-
 		sc.ClearScreen();
-		g.Draw(sc);
-		sc.DrawGroup(0, 0, BOSS_WING_BASE);
-		sc.DrawGroup(0, 0, BOSS_WING_COVER);
-		sc.DrawGroup(30, 0, BOSS_WING_BASE);
-		sc.DrawGroup(30, 0, BOSS_WING_COVER);
-		sc.DrawGroup(16, 0, BOSS_BODY_BASE);
-		sc.DrawGroup(16, 0, BOSS_BODY_COVER);
+		if (g.player_lives >= 0) {
+			g.Update();
+			g.Draw(sc);
+		}
 
 		BeginDrawing();
 		ClearBackground(BLACK);
